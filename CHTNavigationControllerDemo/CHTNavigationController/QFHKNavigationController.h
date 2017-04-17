d46e27e65cf6ae6afc0c18275c63d823d01f0dba
ChanRoy <chen_ht89@163.com>
2017-04-17 16:53:23 +0800

    initialize commit

---
+++ CHTNavigationControllerDemo/CHTNavigationController/QFHKNavigationController.h
@@ -0,0 +1,24 @@
+//
+//  QFHKNavigationController.h
+//  QFangWangHK
+//
+//  Created by cht on 16/3/1.
+//  Copyright © 2016年 qfangwanghk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**
+ *  解决自定义leftBarButton后，系统自带手势返回失效的问题
+ */
+@interface QFHKNavigationController : UINavigationController
+
+@end
+
+@interface UIViewController (CHTNavigationBarHidden)
+
+@property (nonatomic, assign) BOOL cht_prefersNavigationBarHidden;
+
+@property (nonatomic, assign) BOOL cht_interactivePopDisabled;
+
+@end
---
+++ CHTNavigationControllerDemo/CHTNavigationControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CHTNavigationControllerDemo
+//
+//  Created by cht on 2017/4/17.
+//  Copyright © 2017年 cht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
